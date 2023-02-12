1f5ba2d655403a0c0fce348cf955186599915703
Bruno Mota <bmotadev@gmail.com>
2023-02-12 16:45:50 -0300

    estrutura de decisão

---
+++ class1/decision-structure.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  var num1 = 4;
+  var num2 = 4;
+
+  var isTrue = num1 >= num2;
+
+  if(isTrue) {
+    print("Isso é verdadeiro");
+  } else {
+    print("não é verdadeiro");
+  }
+}
