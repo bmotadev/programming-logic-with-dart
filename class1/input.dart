2ad90935619403f9f7c290edb6da84b67a8d53af
Bruno Mota <bmotadev@gmail.com>
2023-02-13 00:25:45 -0300

    aprimorando o algoritmo em dart

--- class1/input.dart
+++ class1/input.dart
@@ -4,9 +4,13 @@ void main(List<String> args) {
     String? input = stdin.readLineSync();
     var idade = int.parse(input ?? '0'); // ?? se o valor da variavel input for null, vai substituir por 0. Isso é o null safety do Dart
 
-    if(idade >= 18){
-      print("maior de idade");
-    } else {
-      print("menor de idade");
+    if(idade >= 50){
+      print("melhor idade");
+    } else if(idade >= 18) {
+      print("adulto de idade");
+    } else if(idade >= 12){
+      print("adolescente");
+    }else {
+      print("criança");
     }
 }
