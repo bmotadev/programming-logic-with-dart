bb88bdb4c13d80d6a654426842fb339dc7572700
Bruno Mota <bmotadev@gmail.com>
2023-02-13 00:22:32 -0300

    adicionado comentario sobre null safety

--- class1/input.dart
+++ class1/input.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 void main(List<String> args) {
   print("=== Digite uma idade ====");
     String? input = stdin.readLineSync();
-    var idade = int.parse(input ?? '0');
+    var idade = int.parse(input ?? '0'); // ?? se o valor da variavel input for null, vai substituir por 0. Isso é o null safety do Dart
 
     if(idade >= 18){
       print("maior de idade");
