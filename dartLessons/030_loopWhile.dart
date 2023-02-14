2a25f2c75fb0b6cba10d0ed174ce2ae881a1ded7
Bruno Mota <bmotadev@gmail.com>
2023-02-14 01:17:30 -0300

    loop while in dart

---
+++ dartLessons/030_loopWhile.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  bool condicao = true;
+  int x = 0;
+
+  while(condicao) {
+    print("rodou $x");
+    if(x > 9) {
+      condicao = false;
+    }
+    x++;
+  }
+}
