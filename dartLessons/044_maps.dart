bfc86ae9406e0aaedcd1e65bf6ccf1898a7a1ec8
Bruno Mota <bmotadev@gmail.com>
2023-02-15 19:36:09 -0300

    typing maps

--- dartLessons/044_maps.dart
+++ dartLessons/044_maps.dart
@@ -1,7 +1,9 @@
 void main(List<String> args) {
   List<String> nomes = ["Bruno", "Mota", "Oliveira"]; // sempre que for lista colocar em plural
   
-  var maps = { // exemplo de map, se assemelha ao JSON. Possivel acessar pelas keys(nomes). Ao inves de acessar por indice como nos arrays/listas
+  // dynamic é um tipo que recebe qualquer coisa
+  // <tipo da key, tipo do que a key vai receber>
+  Map<String, dynamic> maps = { // exemplo de map, se assemelha ao JSON. Possivel acessar pelas keys(nomes). Ao inves de acessar por indice como nos arrays/listas
     "nome" : "Bruno Mota",
     "idade": 29,
     "cidade": "São Paulo",
